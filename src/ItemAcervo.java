fb368cfb84fdf897b6bcd0fb793cb011259a05b8
MatheusGitHubb <silva.soares@academico.ifrn.edu.br>
2016-07-07 17:30:10 -0300

    poo 7/7/16

--- src/ItemAcervo.java
+++
@@ -1,6 +0,0 @@
-import java.util.Date;
-public abstract class ItemAcervo{
-	double custo;
-	boolean pago;
-	Date dataAluguel,dataDevolucao;
-}
--- src/Texto.java
+++
@@ -1,4 +0,0 @@
-
-public class Texto extends ItemAcervo {
-	String autor;
-}
